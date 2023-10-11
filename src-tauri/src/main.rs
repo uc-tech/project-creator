3f7aefecc7ed10009d236a96c1452aa33afa009f
uc-hoba <hoba@ultracombos.com>
2023-10-11 19:52:15 +0800

    chore: add ui and rust method

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,40 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::fs::{self, File};
+use std::process::Command;
+
+#[tauri::command]
+fn create_project(serial: &str, name: &str, path: &str) -> String {
+  if serial.len() == 0 || name.len() == 0 || path.len() == 0 {
+    format!("")
+  } else {
+    let n: String = name.chars().filter(|c| !c.is_whitespace()).collect();
+    let p: String = format!("{}\\{}-{}", path, serial, n);
+    if fs::create_dir_all(p.clone()).is_err() {
+      format!("");
+    }
+
+    let unity_path: String = format!("{}\\Source-Unity-{serial}-{n}", p);
+    fs::create_dir_all(unity_path.clone()).unwrap();
+    fs::create_dir_all(format!("{}\\Project", unity_path)).unwrap();
+    File::create(format!("{}\\Project\\.keep", unity_path)).unwrap();
+    fs::create_dir_all(format!("{}\\Package", unity_path)).unwrap();
+    File::create(format!("{}\\Package\\.keep", unity_path)).unwrap();
+
+    fs::create_dir_all(format!("{}\\Deploy", p)).unwrap();
+
+    Command::new( "explorer" )
+        .arg( p.clone() )
+        .spawn( )
+        .unwrap( );
+    format!("Hello! {}", p)
+  }
+}
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![create_project])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,77 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { invoke } from '@tauri-apps/api/tauri';
+
+	let serial = '20000';
+	let name = '';
+  let path = 'C:\\UC';
+	let message = 'N/A';
+
+	const create = async () => {
+		message = await invoke('create_project', { serial, name, path });
+	};
+</script>
+
+<div class="container mx-auto mt-16">
+	<form class="w-full">
+		<div class="flex flex-wrap mx-3 mb-6">
+			<div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+				<label
+					class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+					for="grid-project-serial"
+				>
+					Project Serial
+				</label>
+				<input
+					class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+					id="grid-project-serial"
+					type="number"
+					min="20000"
+					max="99999"
+					placeholder="20000"
+					bind:value={serial}
+				/>
+			</div>
+			<div class="w-full md:w-1/2 px-3">
+				<label
+					class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+					for="grid-project-name"
+				>
+					Project Name
+				</label>
+				<input
+					class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+					id="grid-project-name"
+					type="text"
+					placeholder="e.g. The Bold"
+          bind:value={name}
+				/>
+			</div>
+		</div>
+		<div class="flex flex-wrap mx-3 mb-6">
+			<div class="w-full px-3">
+				<label
+					class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+					for="grid-path"
+				>
+					Path
+				</label>
+				<input
+					class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+					id="grid-path"
+					type="text"
+					placeholder="e.g. C:\UC"
+          bind:value={path}
+				/>
+				<p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+			</div>
+		</div>
+		<button
+			class="flex flex-wrap mx-6 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+			type="button"
+      on:click="{create}"
+		>
+			Create
+		</button>
+    <p class="mx-6">{message}</p>
+	</form>
+</div>
